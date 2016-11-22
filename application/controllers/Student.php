1581ba61856a9e211577d3a13a24bced427e40d9
dinukanadeeshan <dinuka.nadeeshan@yahoo.com>
2016-11-22 15:35:22 +0530

    Add some other graphs

---
+++ application/controllers/Student.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 2016-11-22
+ * Time: 2:12 PM
+ */
+class Student extends CI_Controller
+{
+
+
+    public function auto_search()
+    {
+        return $this->load;
+    }
+
+}
--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -48,7 +48,7 @@
 
                 <form class="navbar-form navbar-right" role="search">
                     <div class="form-group  is-empty">
-                        <input type="text" class="form-control" placeholder="Search">
+                        <input type="text" class="form-control" id="search" placeholder="Search">
                         <span class="material-input"></span>
                     </div>
                     <button type="submit" class="btn btn-white btn-round btn-just-icon">
@@ -68,7 +68,7 @@
                     <div class="card card-profile">
                         <div class="card-avatar">
                             <a href="#pablo">
-                                <img class="img" src="../assets/img/faces/marc.jpg"/>
+                                <img class="img" src="<?php echo base_url() ?>assets/img/faces/marc.jpg"/>
                             </a>
                         </div>
 
@@ -164,9 +164,8 @@
                             <div id="testchart"></div>
                         </div>
                         <div class="card-content">
-                            <h4 class="title">Daily Sales</h4>
-                            <p class="category"><span class="text-success"><i
-                                        class="fa fa-long-arrow-up"></i> 55%  </span> increase in today sales.</p>
+                            <h4 class="title"></h4>
+                            <!--                            <p class="category">  <span class="text-success"> 50 </span> students </p>-->
                         </div>
                         <div class="card-footer">
                             <div class="stats">
@@ -179,11 +178,11 @@
                 <div class="col-md-6">
                     <div class="card">
                         <div class="card-header card-chart" data-background-color="orange">
-                            <div class="ct-chart" id="emailsSubscriptionChart"></div>
+                            <div id="allmarkschart"></div>
                         </div>
                         <div class="card-content">
-                            <h4 class="title">Email Subscriptions</h4>
-                            <p class="category">Last Campaign Performance</p>
+                            <h4 class="title"></h4>
+                            <p class="category"></p>
                         </div>
                         <div class="card-footer">
                             <div class="stats">
@@ -194,26 +193,11 @@
                     </div>
                 </div>
 
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header card-chart" data-background-color="blue">
-                            <div class="ct-chart" id="completedTasksChart"></div>
-                        </div>
-                        <div class="card-content">
-                            <h4 class="title">Completed Tasks</h4>
-                            <p class="category">Last Campaign Performance</p>
-                        </div>
-                        <div class="card-footer">
-                            <div class="stats">
-                                <i class="material-icons">access_time</i> campaign sent 2 days ago
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
                 <div class="col-md-6">
                     <div class="card">
                         <div class="card-header card-chart" data-background-color="red">
-                            <div class="ct-chart" id="completedTasksChart"></div>
+                            <div id="classavgchart"></div>
                         </div>
                         <div class="card-content">
                             <h4 class="title">Completed Tasks</h4>
@@ -239,6 +223,22 @@
                             <div class="stats">
                                 <i class="material-icons">access_time</i> campaign sent 2 days ago
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header card-chart" data-background-color="blue">
+                        <div id="marksChart"></div>
+                    </div>
+                    <div class="card-content">
+                        <h4 class="title">Completed Tasks</h4>
+                        <p class="category">Last Campaign Performance</p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="stats">
+                            <i class="material-icons">access_time</i> campaign sent 2 days ago
                         </div>
                     </div>
                 </div>
@@ -573,27 +573,37 @@
     </footer>
 </div>
 
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="<?php echo base_url() ?>assets/js/highchart/exporting.js"></script>
+<script src="<?php echo base_url() ?>assets/js/highchart/highcharts.js"></script>
 
 <script>
 
+
     $(function () {
+
+        //autocomplete
+//        $("#search").autocomplete({
+//            source: "<?php //echo base_url(); ?>//index.php/student/auto_search",
+//            minLength: 1
+//        });
+
         Highcharts.chart('testchart', {
             chart: {
                 type: 'line'
             },
             title: {
-                text: 'Place in the class'
+                text: 'Rank in the class'
             },
             subtitle: {
-                text: 'out of 40'
+                text: 'out of 40 students'
             },
             xAxis: {
                 categories: ['Grade-6', 'Grade-7', 'Grade-8', 'Grade-9', 'Grade-10', 'Grade-11']
             },
             yAxis: {
                 reversed: true,
+                min: 1,
+
                 title: {
                     text: 'Place '
                 }
@@ -610,6 +620,215 @@
                 name: ' ',
                 data: [23, 19, 15, 14, 10]
             },]
+        });
+
+        Highcharts.chart('marksChart', {
+            title: {
+                text: 'Average marks for each subject',
+                x: -20 //center
+            },
+            subtitle: {
+                text: null,
+                x: -20
+            },
+            xAxis: {
+                title: {
+                    text: 'Grades'
+                },
+                categories: ['Grade-6', 'Grade-7', 'Grade-8', 'Grade-9', 'Grade-10', 'Grade-11']
+            },
+            yAxis: {
+                min: 0,
+                max: 100,
+                title: {
+                    text: 'Marks (%)'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            // tooltip: {
+            //     valueSuffix: '°C'
+            // },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: [{
+                name: 'Sinhala',
+                data: [25, 30, 95, 45, 82, 25]
+            }, {
+                name: 'Maths',
+                data: [52, 65, 33, 83, 39, 96]
+            }, {
+                name: 'Buddhism',
+                data: [91, 62, 35, 84, 75, 70]
+            }, {
+                name: 'History',
+                data: [86, 79, 43, 90, 59, 78]
+            }, {
+                name: 'Science',
+                data: [86, 79, 43, 90, 39, 99]
+            }, {
+                name: 'English',
+                data: [92, 64, 35, 54, 35, 70]
+            }, {
+                name: 'Art',
+                data: [23, 13, 24, 45, 34, 23]
+            }, {
+                name: 'Health',
+                data: [78, 89, 79, 80, 90, 100]
+            }, {
+                name: 'Tech. Skills',
+                data: [null, null, null, null, 56, 70]
+            }]
+        });
+
+
+        Highcharts.chart('allmarkschart', {
+            chart: {
+                type: 'bar',
+                inverted: true,
+                polar: false
+            },
+            title: {
+                text: 'All marks in Grade 10'
+            },
+            subtitle: {
+                text: ''
+            },
+            xAxis: {
+
+                categories: ['sinhala', 'Maths', 'Buddhism', 'History', 'English', 'Science', 'Art', 'Health', 'Tech.Skills'],
+                title: {
+                    text: 'Subjects'
+                }
+            },
+            yAxis: {
+                min: 0,
+                max: 100,
+                title: {
+                    text: 'Marks (%) ',
+                    align: 'high'
+                },
+                labels: {
+                    overflow: 'justify'
+                }
+            },
+            tooltip: {
+                valueSuffix: ' %'
+            },
+            plotOptions: {
+                bar: {
+                    dataLabels: {
+                        enabled: false
+                    }
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'top',
+                x: -40,
+                y: 80,
+                floating: true,
+                borderWidth: 1,
+                backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+                shadow: true
+            },
+            credits: {
+                enabled: false
+            },
+            series: [{
+                name: 'First Term',
+                data: [17, 31, 63, 63, 68, 80, 56, 78, 67]
+            }, {
+                name: 'Second term',
+                data: [33, 56, 97, 48, 69, 84, 45, 79, 98]
+            }, {
+                name: 'Third term',
+                data: [52, 54, 50, 74, 38, 88, 34, 56, 23]
+            }]
+        });
+
+
+        Highcharts.chart('classavgchart', {
+            chart: {
+                zoomType: 'xy'
+            },
+            title: {
+                text: 'Marks of student with class average'
+            },
+            subtitle: {
+                text: ''
+            },
+            xAxis: [{
+                categories: ['sinhala', 'Maths', 'Buddhism', 'History', 'English', 'Science', 'Art', 'Health', 'Tech.Skills'],
+                crosshair: true
+            }],
+            yAxis: [{ // Primary yAxis
+                min: 0,
+                max: 100,
+                labels: {
+                    format: '{value}',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                },
+                title: {
+                    text: 'Class Average',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                }
+            }, { // Secondary yAxis
+                min: 0,
+                max: 100,
+                title: {
+                    text: 'Marks of student',
+                    style: {
+                        color: Highcharts.getOptions().colors[0]
+                    }
+                },
+                labels: {
+                    format: '{value}',
+                    style: {
+                        color: Highcharts.getOptions().colors[0]
+                    }
+                },
+                opposite: true
+            }],
+            tooltip: {
+                shared: true
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'left',
+                x: 120,
+                verticalAlign: 'middle',
+                y: 100,
+                floating: true,
+                backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+            },
+            series: [{
+                name: 'Marks of student',
+                type: 'column',
+                yAxis: 1,
+                data: [46, 71, 64, 92, 44, 76, 56, 85, 64],
+
+
+            }, {
+                name: 'Class Average',
+                type: 'spline',
+                data: [50, 69.5, 53.2, 45.3, 42.6, 67.6, 52, 65, 39.9],
+                tooltip: {
+                    valueSuffix: null
+                }
+            }]
         });
     });
 </script>
